428db909375608c03ebc6a23e6b53ca8be4e3451
Aman <119286791+amantyagi23@users.noreply.github.com>
2024-03-19 15:02:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+var name = "Aman Tyagi";
+
+// function
+
+// function programing language
+
+// function function_Name(arguments) {
+// function body
+// }
+
+// function_Name(argument)
+
+// function alwasy called
+
+// function printHello(name) {
+//   console.log(name);
+// }
+
+// printHello(true);
+
+// printHello("Aman Tyagi");
+
+// printHello(12);
+
+// even and odd bt 1 to 10
+
+// function EvenOdd() {
+//   //   console.log(1);
+//   //   console.log(2);
+//   //   console.log(3);
+//   for (var i = 0; i < 10; i = i + 2) {
+//     console.log(i);
+//   }
+// }
+
+// EvenOdd();
+
+// dom work
+
+var body = document.getElementById("hero");
+
+body.style.display = "none";
+console.log(body);
